fab7fc39d0bcd6d9062ca9663ce2dc0c0249e813
vineeth <vineeth@pcim.com.au>
2016-07-21 16:28:29 +1000

    Ticket stats model

---
+++ api/models/ticket_count_by_enquiry_type.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+module.exports = {
+  connection: 'Mysql_1300',
+  hookTimeout: 50000,
+  attributes: {
+	enquiry_type:{
+		type:'integer',
+		primaryKey: true,
+		size:10,
+		required:true
+	},
+	nos:{
+		type:'integer',
+                size:30
+	}
+
+
+  }
+};
